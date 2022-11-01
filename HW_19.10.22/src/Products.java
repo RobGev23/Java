e92d706d2d5967fe2d429375e9e38a7fab896b75
Robert Gevorkian <robgev23@gmail.com>
2022-11-01 18:47:56 +0100

    deleted

--- HW_19.10.22/src/Products.java
+++
@@ -1,6 +0,0 @@
-public class Products extends Bill {
-    public Products(String productName, double price, double urgentDelivery, Bill receipts) {
-        super(productName, price, urgentDelivery);
-    }
-
-}
